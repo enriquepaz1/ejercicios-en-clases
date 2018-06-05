d0640862308e8d13bf3c57f3123cf6ed86d00ca7
enrique2204 <enrique.bayron.paz@gmail.com>
2018-06-05 09:09:01 -0400

    enrique paz1

--- src/operaciones_aritmeticas.java
+++ src/operaciones_aritmeticas.java
@@ -3,9 +3,36 @@
  */
 public class operaciones_aritmeticas {
      public static void main(String[] args) {
+         //unit test
+    operaciones_aritmeticas op= new operaciones_aritmeticas();
+
+         op.suma(3,5);
+         op.suma(3,-5);
+         op.resta (2,5);
+         op.resta (-3,-5);
+
+//assertEqual (expected 54 , operaciones_aritmeticas.suma (a:34,b:20);
+            //unit test
+         //suma (3,5);
+
+         //suma(3,-5);
+         //resta ()
+
      }
-    public static int suma (int a,int b){
-        return a+b;
+
+    public static int suma (int a,int b) {
+        return a + b;
     }
-        
+    public static int resta (int a,int b) {
+        return a - b;
+    }
+
+    public static double multiplicacion (int a, int b) {
+        return a*b;
+    }
+
+
+
+
+
      }
---
+++ test/test.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Personal on 05/06/2018.
+ */
+public class test {
+    public static void main(String[] args) {
+
+}
