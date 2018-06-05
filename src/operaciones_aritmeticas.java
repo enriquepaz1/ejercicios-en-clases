c1434859d6938e96255add8a42927632a44c37e5
enrique2204 <enrique.bayron.paz@gmail.com>
2018-06-05 08:28:30 -0400

    enrique paz

---
+++ src/operaciones_aritmeticas.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Personal on 05/06/2018.
+ */
+public class operaciones_aritmeticas {
+     public static void main(String[] args) {
+     }
+    public static int suma (int a,int b){
+        return a+b;
+    }
+        
+     }
