f5cc38882782e5e6bf12db62243f4f1e7d9e3043
Kevinsan94 <kevinviscafe@gmail.com>
2018-06-05 08:33:25 -0400

    Kevin Viscafe

--- src/operaciones_aritmeticas.java
+++ src/operaciones_aritmeticas.java
@@ -2,10 +2,20 @@
  * Created by Personal on 05/06/2018.
  */
 public class operaciones_aritmeticas {
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
+
      }
-    public static int suma (int a,int b){
+
+     public static int suma (int a,int b){
         return a+b;
     }
-        
-     }
+
+    public static int resta(int a, int b){
+        return a-b;
+    }
+
+    public static int multiplicacion(int a, int b){
+        return a*b;
+    }
+}
