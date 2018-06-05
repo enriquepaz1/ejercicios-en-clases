b4bd6fd680b39f3cc2ae783e36bbc44c427fd35c
MonoMatheus <monkeymon1313@gmail.com>
2018-06-05 08:46:35 -0400

    Matheus

--- src/operaciones_aritmeticas.java
+++ src/operaciones_aritmeticas.java
@@ -6,6 +6,25 @@ public class operaciones_aritmeticas {
      }
     public static int suma (int a,int b){
         return a+b;
-    }
+
         
      }
+
+
+    public int potencia (int a) {
+
+        int pot = a*a;
+
+        return pot;
+
+    }
+
+    public double div(int a,int b) {
+
+        double div = a/b;
+
+        return div;
+
+
+    }
+}
